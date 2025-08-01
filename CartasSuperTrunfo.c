10e88d1ae71c952e65bb5c8f95f0ae633a04abd3
Alan-estacio <alan.nunes15ti@gmail.com>
2025-08-01 00:14:07 +0000

    modigiquei o arquivo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,6 +21,7 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    printf("Carta 1");
+    printf("Cartaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n");
+    printf("sem teste agora");
     return 0;
 }
